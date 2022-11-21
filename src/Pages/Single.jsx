714c11f3620bae758652eb7cd478ddbfc29cd152
Mendheys <97525279+Mendheys@users.noreply.github.com>
2022-11-21 18:19:39 +0530

    submitted

---
+++ src/Pages/Single.jsx
@@ -0,0 +1,19 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Single = () => {
+  const data = JSON.parse(localStorage.getItem("fastor-single"));
+  console.log(data);
+  return (
+    // <Box backgroundImage={`${data.images[0].url}`}>
+
+    // </Box>
+    <Box  padding={"20px"}>
+        <Text fontSize='2xl' as='b'>{data.restaurant_name}</Text>
+      <Image src={data.images[0].url} alt="Image" />
+      <img src="../Images/Fastor Logo.png" alt="Fastor"/>
+    </Box>
+  );
+};
+
+export default Single;
